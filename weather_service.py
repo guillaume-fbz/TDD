0d73b597f43e001bf5dc2e8e4008275a2454fd82
Guillaume Fabrizi <46895262+guillaume-fbz@users.noreply.github.com>
2024-01-20 19:06:27 +0100

    Update weather_service.py

--- weather_service.py
+++ weather_service.py
@@ -6,8 +6,8 @@ class WeatherService:
     def __init__(self):
         self.api_key = "ASK ME FOR THE API_KEY :)"
 
-    def get_temperature(self, town):
+    def get_temperature(self, city):
         response = requests.get(
-            f"https://api.tomorrow.io/v4/weather/realtime?location={town}&apikey={self.api_key}")
+            f"https://api.tomorrow.io/v4/weather/realtime?location={city}&apikey={self.api_key}")
         return response.json()['data']['values']['temperature']
 
